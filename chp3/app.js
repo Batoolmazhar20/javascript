100aeb7d53e8cd91e499d4bdcf0de890af6c00ee
Batoolmazhar20 <batoolmazhar5@gmail.com>
2025-12-22 19:07:39 +0500

    Added chp3

---
+++ chp3/app.js
@@ -0,0 +1,17 @@
+var age = "I am 20 Years old"
+console.log(age)
+alert(age)
+
+var visit = " 14 "
+document.write("You have visited this site " + visit + "times<br>")
+
+
+var birthYear = "2005"
+document.write("My Birth year is " + birthYear + "<br>")
+
+
+var customerName = "Jhon Doe"
+var customerShirt = "5 T-shirts"
+var customerStore = "XYZ clothing store"
+
+document.write(customerName + " ordered " + customerShirt + " from " + customerStore)
