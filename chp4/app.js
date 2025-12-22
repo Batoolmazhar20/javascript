73a450fc19702000d2faa6564d9e07ab3857a1a1
Batoolmazhar20 <batoolmazhar5@gmail.com>
2025-12-22 23:18:56 +0500

    Added chp4 chapter

---
+++ chp4/app.js
@@ -0,0 +1,27 @@
+//Declare 3 variables in one statement//
+//var name = firstName, last$Name, _age //
+
+//Declare 5 legal names//
+//var userName, _visit, my_Name//
+
+//Display in browser//
+
+var rules = "Rules for naming JS variables:<br>"
+console.log("rules")
+document.write(rules)
+
+var varNames = "Variables name can only contain, numbers, $ ,and _. For example : $mymy_1st Variable<br>"
+console.log("varNames")
+document.write(varNames)
+
+var varLetters = "Variables must begin with a letter, $ or _.<br>"
+console.log("varLetters")
+document.write(varLetters)
+
+var varcase = "Variable names are case sensitive<br>"
+console.log("varcase")
+document.write(varcase)
+
+var keyWords = "Variable names should not be JS keywords"
+console.log("keyWords")
+document.write(keyWords)
