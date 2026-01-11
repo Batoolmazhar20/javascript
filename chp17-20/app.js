f298a1f953863a6aa7d28694ffcfd58c7bf62260
Batoolmazhar20 <batoolmazhar5@gmail.com>
2026-01-11 19:27:17 +0500

    Added chp17-20

--- chp17-20/app.js
+++ chp17-20/app.js
@@ -34,3 +34,103 @@ for (var i = 0; i < fruits.length; i++) {
     document.write("Element at index " + i + " is " + fruits[i] + "<br>");
 }
 
+
+
+
+
+
+document.write("<b>Counting:</b><br>");
+for (let i = 1; i <= 15; i++) {
+  document.write(i + ", ");
+}
+document.write("<br><br>");
+
+document.write("<b>Reverse counting:</b><br>");
+for (let i = 10; i >= 1; i--) {
+  document.write(i + ", ");
+}
+document.write("<br><br>");
+
+
+document.write("<b>Even:</b><br>");
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    document.write(i + ", ");
+  }
+}
+document.write("<br><br>");
+
+document.write("<b>Odd:</b><br>");
+for (let i = 1; i < 20; i++) {
+  if (i % 2 !== 0) {
+    document.write(i + ", ");
+  }
+}
+document.write("<br><br>");
+
+document.write("<b>Series:</b><br>");
+for (let i = 2; i <= 20; i++) {
+  if (i % 2 === 0) {
+    document.write(i + "k, <br>");
+  }
+}
+
+
+
+
+
+
+var bakeryItems = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+userInput = userInput.toLowerCase();
+
+var found = false;
+
+
+for (var i = 0; i < bakeryItems.length; i++) {
+
+  if (bakeryItems[i] === userInput) {
+    found = true;
+  }
+}
+
+
+if (found === true) {
+  document.write(userInput + " is <b>available</b> in our bakery");
+} else {
+  document.write("We are sorry. " + userInput + " is <b>not available</b> in our bakery<br>");
+}
+
+
+
+
+
+var arr = [24, 53, 78, 91, 12];
+
+document.write("<b>Array items:</b> " + arr + "<br>");
+
+var largest = arr[0];   
+for (var i = 1; i < arr.length; i++) {
+  if (arr[i] > largest) {
+    largest = arr[i];
+  }
+}
+
+document.write("<b>The largest number is</b> " + largest + "<br>");
+
+
+
+var arr = [24, 58, 78, 91, 12];
+
+document.write("<b>Array items:</b> " + arr + "<br>");
+
+var smallest = arr[0];   
+
+for (var i = 1; i < arr.length; i++) {
+  if (arr[i] < smallest) {
+    smallest = arr[i];
+  }
+}
+
+document.write("<b>The smallest number is</b> " + smallest + "<br>");
