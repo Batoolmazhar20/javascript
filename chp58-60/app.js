d0e5b39b9b59be217a46c8e8621585015e65f9e7
Batoolmazhar20 <batoolmazhar5@gmail.com>
2026-03-07 22:15:55 +0500

    Updated

--- chp58-60/app.js
+++ chp58-60/app.js
@@ -9,3 +9,26 @@ for(var i=0 ; i<renderElements.length ; i++){
 document.getElementById("first-name").value="Alex";
 document.getElementById("last-name").value="Bank";
 document.getElementById("email").value="alexbank@wexample.com";
+
+
+var element = document.getElementById("form-content")
+console.log(element.nodeType)
+
+var lastName = document.getElementById("lastName")
+console.log(lastName.nodeType)
+console.log(lastName.childNodes[0].nodeType)
+
+var lastName = document.getElementById("lastName")
+lastName.childNodes[0].nodeValue = "last Name: Mazhar"
+
+var mainContent = document.getElementById("main-content")
+console.log(mainContent.firstChild)
+console.log(mainContent.lastChild)
+
+var lastName = document.getElementById("lastName")
+console.log(lastName.previousElementSibling)
+console.log(lastName.nextElementSibling)
+
+var email = document.getElementById("email")
+console.log(email.parentNode)
+console.log(email.parentNode.nodeType)
